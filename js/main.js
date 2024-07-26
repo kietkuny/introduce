7fbcc1d49afcdbf41036fa6f5521eeaf85d85b02
Kiet Kun <dotuankiet00@gmail.com>
2024-07-26 15:03:21 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(window).on('scroll', function() {
+  let header = $('header');
+  if ($(this).scrollTop() > 0) {
+      header.addClass('bg-body-tertiary border-bottom border-dark ');
+  } else {
+      header.removeClass('bg-body-tertiary border-bottom border-dark');
+  }
+});
+$(`.hamburger-menu`).on("click", function () {
+  $(`.hamburger-menu`).toggleClass('change');
+  $(`.menu-header`).toggleClass('menu-header-close');
+})
+
+$('.banner-content-left').slick({
+  dots: false,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+  pauseOnFocus: false,
+  loop: true,
+  autoplaySpeed: 2000,
+});
+		
