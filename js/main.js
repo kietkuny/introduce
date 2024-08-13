48696ab99e1a78d5eed8fb9c01d74a964b66a8ae
Kiet Kun <dotuankiet00@gmail.com>
2024-08-13 11:14:53 +0700

    update

--- js/main.js
+++ js/main.js
@@ -10,6 +10,45 @@ $(`.hamburger-menu`).on("click", function () {
   $(`.hamburger-menu`).toggleClass('change');
   $(`.menu-header`).toggleClass('menu-header-close');
 })
+
+$("#copyMomo").click(function(){
+  // Lấy nội dung từ thẻ p
+  let text = $("#sdtmomo").text();
+
+  // Tạo một phần tử input tạm thời để sao chép
+  let tempInput = $("<input>");
+  $("main").append(tempInput);
+  tempInput.val(text).select();
+  
+  // Thực hiện lệnh sao chép
+  document.execCommand("copy");
+  
+  // Xóa phần tử input tạm thời
+  tempInput.remove();
+
+  // Hiển thị nội dung đã sao chép trong modal
+  $("#copiedMomo").text(text);
+  $("#copyModalMomo").modal('show');
+});
+$("#copyVietcom").click(function(){
+  // Lấy nội dung từ thẻ p
+  let text = $("#stkvietcom").text();
+
+  // Tạo một phần tử input tạm thời để sao chép
+  let tempInput = $("<input>");
+  $("main").append(tempInput);
+  tempInput.val(text).select();
+  
+  // Thực hiện lệnh sao chép
+  document.execCommand("copy");
+  
+  // Xóa phần tử input tạm thời
+  tempInput.remove();
+
+  // Hiển thị nội dung đã sao chép trong modal
+  $("#copiedVietcom").text(text);
+  $("#copyModalVietcom").modal('show');
+});
 // $(window).on('load', function(){
 //   $('.banner-content-left').slick({
 //     dots: false,
